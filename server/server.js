4554bf5c333bfa79cdb4c3b44963adca5896c0bd
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-10 11:58:25 +0200

    add csrf

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const cookieSession = require("cookie-Session");
 const app = express();
+const csurf = require("csurf");
 const compression = require("compression");
 const { sessionSecret } = require("./secrets.json");
 const path = require("path");
 const { saveUser } = require("./db_queries");
+const { loginUser } = require("./functions");
 
 app.use(compression());
 
+//express public folder
 app.use(express.static(path.join(__dirname, "..", "client", "public")))
     .use(express.json())
     .use(
@@ -17,7 +20,14 @@ app.use(express.static(path.join(__dirname, "..", "client", "public")))
         })
     );
 
-app.get("/user/id.json", function (request, response) {
+//CSRF Token
+app.use(csurf());
+app.use(function (request, response, next) {
+    response.cookie("myCsrfToken", request.csrfToken());
+    next();
+});
+
+app.get("/user/id.json", (request, response) => {
     console.log("...(GET /user/id.json) request.session: ", request.session);
 
     response.json({
@@ -25,16 +35,36 @@ app.get("/user/id.json", function (request, response) {
     });
 });
 
-app.post("/api/register", function (request, response) {
+app.post("/api/register", (request, response) => {
     console.log("...(POST /api/register) request.body: ", request.body);
-    saveUser({ ...request.body }).then((user) => {
-        console.log("(.then) user.rows ID: ", user.rows[0].id);
-        request.session.userId = user.rows[0].id;
-        console.log("(.then) request.session.userId: ", request.session.userId);
-        response.json(user);
-    });
+    saveUser({ ...request.body })
+        .then((user) => {
+            console.log("(.then) user.rows ID: ", user.rows[0].id);
+            request.session.userId = user.rows[0].id;
+            console.log(
+                "(.then) request.session.userId: ",
+                request.session.userId
+            );
+            response.json(user);
+        })
+        .catch((error) => {
+            console.log("Error...blabla: ", error);
+            response.json({ error: "Registration failed." });
+        });
 });
-
+app.post("/api/login", (request, response) => {
+    console.log("...(POST /api/login) request.body: ", request.body);
+    loginUser({ ...request.body })
+        .then((user) => {
+            request.session.userId = user.rows[0].id;
+            response.json(user);
+        })
+        .catch((error) => {
+            console.log("Error POST /api/login: ", error);
+            response.statusCode = 400;
+            response.json({ error: "Login failed - wrong credentials!" });
+        });
+});
 app.get("*", function (request, response) {
     response.sendFile(path.join(__dirname, "..", "client", "index.html"));
 });
