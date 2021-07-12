2e96ff4e9c86049b1c4bc36cfc1480c176df339c
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-12 15:34:53 +0200

    implement App component for logged in part of the app

--- client/src/start.js
+++ client/src/start.js
@@ -1,20 +1,12 @@
 import ReactDOM from "react-dom";
 import axios from "./axios";
 import Welcome from "./components/Welcome";
-import { Link } from "react-router-dom";
+import App from "./components/App";
 
 axios.get("/user/id.json").then(({ data }) => {
-    console.log("...(start.js axios GET /user/id.json) result.data: ", data);
     if (!data.userId) {
         ReactDOM.render(<Welcome />, document.querySelector("main"));
     } else {
-        ReactDOM.render(
-            <div>
-                <img className="logo" src="img/logo.png" alt="logo" />
-                <p>Hello World! Logged in!</p>
-                <a href="/logout">Logout</a>
-            </div>,
-            document.querySelector("main")
-        );
+        ReactDOM.render(<App />, document.querySelector("main"));
     }
 });
