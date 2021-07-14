29d12718d9fd0a4f977e05eae0f9f9b6f0f88b57
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-14 17:16:33 +0200

    display other users bio, image etc

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import ProfilePic from "./ProfilePic";
 import axios from "../axios";
 
 export default class UserProfile extends Component {
@@ -15,18 +16,35 @@ export default class UserProfile extends Component {
 
         // bind methods here!
     }
-    componentDidMount() {
+    async componentDidMount() {
         console.log("...(UserProfile: componentDidMount)");
+        // get id from url --> this.props.match.params.id
+        const userId = this.props.match.params.id;
+        const otherUser = await axios.get(`/api/user/${userId}`);
+        console.log(
+            "...(UserProfile: componentDidMount) otherUser: ",
+            otherUser.data
+        );
+        this.setState(otherUser.data);
+        console.log("STATE: ", this.state);
     }
     render() {
+        const { first_name, last_name, email, bio, id, profile_url } =
+            this.state;
+
         return (
             <div className="profileWrapper">
                 <ProfilePic
                     first_name={first_name}
                     last_name={last_name}
                     profile_url={profile_url}
+                    bio={bio}
                     className="bigProfilePic"
                 ></ProfilePic>
+                <div className="bioContent">
+                    <h1>Bio</h1>
+                    <p>{bio}</p>
+                </div>
             </div>
         );
     }
