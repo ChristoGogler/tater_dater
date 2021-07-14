4dba1875f8100b13a847a7f0d88fb67a76cf53b0
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-14 17:18:07 +0200

    change sqlsetup file, add Insert statement for fake users

--- client/src/start.js
+++ client/src/start.js
@@ -3,7 +3,7 @@ import axios from "./axios";
 import Welcome from "./components/Welcome";
 import App from "./components/App";
 
-axios.get("/user/id.json").then(({ data }) => {
+axios.get("/api/user/id.json").then(({ data }) => {
     if (!data.userId) {
         ReactDOM.render(<Welcome />, document.querySelector("main"));
     } else {
