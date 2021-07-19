fb3705313fd95e14862d7b2c33d75c6f86d83381
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-19 11:04:41 +0200

    refactoring

--- server/routehandler.js
+++ server/routehandler.js
@@ -22,28 +22,39 @@ const csrfToken = (request, response, next) => {
 //FIND PROFILES
 const findProfiles = async (request, response) => {
     const { q } = request.query;
-    console.log("...(findProfiles) q.length: ", q.length);
     if (q.length < 3) {
         return;
     }
-    const profiles = await getUserProfiles(q);
-    console.log("...(findProfiles) result", profiles);
-    if (profiles.length == 0) {
-        console.log("...(findProfiles) No user Found!");
-        response.status(404).json({ message: "No users found!" });
+    try {
+        const profiles = await getUserProfiles(q);
+        if (profiles.length == 0) {
+            console.log("...(findProfiles) No user Found!");
+            response.status(404).json({ message: "No users found!" });
+        }
+        response.json(profiles);
+    } catch (error) {
+        console.log("ERROR fetching users: ", error);
+        response.json({
+            error: "Problem fetching users.",
+        });
     }
-    response.json(profiles);
 };
 
 //FIND LATEST PROFILES
 const findLatestProfiles = async (request, response) => {
-    const profiles = await getLatestUserProfiles();
-    console.log("...(findLatestProfiles) result: ", profiles);
-    if (profiles.length == 0) {
-        console.log("...(findProfiles) No user Found!");
-        response.status(404).json({ message: "No users found!" });
+    try {
+        const profiles = await getLatestUserProfiles();
+        if (profiles.length == 0) {
+            console.log("...(findProfiles) No user Found!");
+            response.status(404).json({ message: "No users found!" });
+        }
+        response.json(profiles);
+    } catch (error) {
+        console.log("ERROR fetching most recent users: ", error);
+        response.json({
+            error: "Problem fetching most recent users.",
+        });
     }
-    response.json(profiles);
 };
 
 //GET FRIENDSHIP STATUS
@@ -52,13 +63,13 @@ const getFriendStatus = async (request, response) => {
     const { user_id: user2_id } = request.params;
     try {
         const result = await getFriendshipStatus({ user1_id, user2_id });
-        console.log("...(RH: getFriendStatus) result: ", result.friend_status);
         response.json({
             status: result.friend_status,
             sender: result.sender_id,
             recipient: result.recipient_id,
         });
     } catch (error) {
+        console.log("Not a friend. userId: ", user2_id);
         response.json({ status: null });
     }
 };
@@ -68,40 +79,46 @@ const changeFriendStatus = async (request, response) => {
     console.log("...(RH: changeFriendStatus)");
     const { userId: user1_id } = request.session;
     const { action, user2_id } = request.query;
-    console.log(action, user1_id, user2_id);
     let result;
-    switch (action) {
-        case "request":
-            result = await saveFriendrequest({ user1_id, user2_id });
-            break;
-        case "accept":
-            result = await updateFriendstatus({
-                user1_id,
-                user2_id,
-                friend_status: "friends",
-            });
-            break;
-        case "cancel":
-            result = await deleteFriend({ user1_id, user2_id });
-            break;
-        case "unfriend":
-            result = await deleteFriend({ user1_id, user2_id });
-            break;
-        default:
-            break;
+    try {
+        switch (action) {
+            case "request":
+                result = await saveFriendrequest({ user1_id, user2_id });
+                break;
+            case "accept":
+                result = await updateFriendstatus({
+                    user1_id,
+                    user2_id,
+                    friend_status: "friends",
+                });
+                break;
+            case "cancel":
+                result = await deleteFriend({ user1_id, user2_id });
+                break;
+            case "unfriend":
+                result = await deleteFriend({ user1_id, user2_id });
+                break;
+            default:
+                break;
+        }
+        response.json({
+            status: result.friend_status,
+            sender: result.sender_id,
+            recipient: result.recipient_id,
+        });
+    } catch (error) {
+        console.log("ERROR updating friendship status: ", error);
+        response.json({
+            error: "Problem updating friendship status.",
+        });
     }
-    response.json({
-        status: result.friend_status,
-        sender: result.sender_id,
-        recipient: result.recipient_id,
-    });
 };
 
 //GET MY PROFILE
-const getMyProfile = (request, response) => {
+const getMyProfile = async (request, response) => {
     const { userId } = request.session;
-    getUserById(userId).then((user) => {
-        console.log("...(GET /api/user) USER: ", user);
+    try {
+        const user = await getUserById(userId);
         response.json({
             id: user.id,
             first_name: user.first_name,
@@ -110,131 +127,149 @@ const getMyProfile = (request, response) => {
             profile_url: user.profile_url,
             bio: user.bio,
         });
-    });
+    } catch (error) {
+        console.log("ERROR fetching user profile: ", error);
+        response.json({
+            error: "Problem fetching user profile. ",
+        });
+    }
 };
 
 //GET USER PROFILE
 const getUserProfile = async (request, response) => {
-    console.log("...(GET /api/user/:id)", request.params.id);
     const id = request.params.id;
     if (id == request.session.userId) {
         response.json({ self: true });
         return;
     }
-    const user = await getUserById(id);
-    console.log("...(GET /api/user/:id) otherUser: ", user);
-    if (!user) {
-        console.log("no user found!");
-        response.json(null);
-        return;
+    try {
+        const user = await getUserById(id);
+        if (!user) {
+            console.log("no user found!");
+            response.json(null);
+            return;
+        }
+        response.json(user);
+    } catch (error) {
+        console.log("ERROR fetching user profile: ", error);
+        response.json({
+            error: "Problem fetching user profile. ",
+        });
     }
-    response.json(user);
 };
 
 //CHECK IF LOGGED IN
 const checkLogin = (request, response) => {
-    console.log("...(GET /user/id.json) request.session: ", request.session);
     response.json({
         userId: request.session.userId,
     });
 };
 
 //LOGIN
-const login = (request, response) => {
-    console.log("...(POST /api/login) request.body: ", request.body);
-    loginUser({ ...request.body })
-        .then((user) => {
-            //TODO
-            console.log("...(POST / api / login) user: ", user);
-            request.session.userId = user.id;
-            response.json(user);
-        })
-        .catch((error) => {
-            console.log("Error POST /api/login: ", error);
-            response.statusCode = 400;
-            response.json({ error: "Login failed - wrong credentials!" });
-        });
+const login = async (request, response) => {
+    try {
+        const user = await loginUser({ ...request.body });
+        request.session.userId = user.id;
+        response.json(user);
+    } catch (error) {
+        console.log("ERROR logging in: ", error);
+        response.statusCode = 401;
+        response.json({ error: "Login failed - wrong credentials!" });
+    }
 };
 
 //LOGOUT
 const logout = (request, response) => {
-    console.log("...(POST /api/logout) request: ", request.body);
     request.session.userId = null;
-    console.log("...(POST /api/logout) userId after: ", request.session.userId);
     response.json({ message: "You've been logged out successfully!" });
 };
 
 //REGISTER
-const register = (request, response) => {
-    console.log("...(POST /api/register) request.body: ", request.body);
-    saveUser({ ...request.body })
-        .then((user) => {
-            request.session.userId = user.rows[0].id;
-            response.statusCode = 200;
-            sendRegistrationMail(user.rows[0]);
-            response.json(user);
-        })
-        .catch((error) => {
-            console.log("Error...blabla: ", error);
-            response.statusCode = 400;
-            response.json({
-                error: "There already exists an account with this email.",
-            });
+const register = async (request, response) => {
+    try {
+        const user = await saveUser({ ...request.body });
+        request.session.userId = user.rows[0].id;
+        response.statusCode = 200;
+        sendRegistrationMail(user.rows[0]);
+        response.json(user);
+    } catch (error) {
+        console.log("ERROR saving user with this email: ", error);
+        response.json({
+            error: "Problem saving user with this email.",
         });
+    }
 };
 
 //PASSWORD  RESET
-const resetPassword_step1 = (request, response) => {
-    console.log("...(POST /password/reset/step1)");
-    verifyEmail({ ...request.body }).then((isVerified) => {
+const resetPassword_step1 = async (request, response) => {
+    try {
+        const isVerified = await verifyEmail({ ...request.body });
         if (isVerified) {
             response.json({ isVerified: true });
             return;
         }
         response.statusCode = 400;
-        response.json({ error: "Verification failed - email not registered!" });
-    });
+        response.json({
+            error: "Verification failed - email not registered!",
+        });
+    } catch (error) {
+        console.log("ERROR verifying email: ", error);
+        response.statusCode(500).json({
+            error: "Problem verifying email: " + error,
+        });
+    }
 };
-const resetPassword_step2 = (request, response) => {
-    console.log("...(POST /password/reset/step2)");
-    getCodeByEmail({ ...request.body }).then((result) => {
-        console.log(
-            "Code, request.body",
-            result.secret_code,
-            request.body.code
-        );
+const resetPassword_step2 = async (request, response) => {
+    try {
+        const result = await getCodeByEmail({ ...request.body });
         if (result.secret_code == request.body.code) {
-            saveNewPassword({ ...request.body }).then((user) => {
+            try {
+                const user = await saveNewPassword({ ...request.body });
                 response.json({ user: user });
-            });
+            } catch (error) {
+                console.log("ERROR saving new password: ", error);
+                response
+                    .statusCode(500)
+                    .json({ error: "Problem saving new password: " + error });
+            }
         }
-    });
+    } catch (error) {
+        console.log("ERROR fetching secret code from database: ", error);
+        response.statusCode(500).json({
+            error: "Problem fetching secret code from database: " + error,
+        });
+    }
 };
 
 // SAVE NEW BIO
-const saveNewBio = (request, response) => {
-    console.log("...(PUT /api/user/update/bio)", request.body);
+const saveNewBio = async (request, response) => {
     const { userId } = request.session;
     const { bio } = request.body;
-    saveUserBio({ bio, userId })
-        .then((result) => {
-            console.log("...(PUT /api/user/update/bio) result: ", result);
-            response.json(result);
-        })
-        .catch((error) => {
-            console.log("Error saving user bio: ", error);
-        });
+    try {
+        const result = await saveUserBio({ bio, userId });
+        response.json(result);
+    } catch (error) {
+        console.log("ERROR saving user bio: ", error);
+        response
+            .statusCode(500)
+            .json({ error: "Problem saving new bio: " + error });
+    }
 };
 
 //UPLOAD PROFILE PICTURE
-const saveProfilePictureUrl = (request, response) => {
-    console.log("...(POST /api/upload) request.file: )", request.file);
-    // console.log("...(POST /api/upload)) response: ", response);
-    saveProfileUrl({ ...request.body, ...request.session }).then((result) => {
-        console.log("...(POST api/upload saveProfileUrl) result: ", result);
+const saveProfilePictureUrl = async (request, response) => {
+    try {
+        const result = await saveProfileUrl({
+            ...request.body,
+            ...request.session,
+        });
         response.json({ user: result });
-        return;
-    });
+    } catch (error) {
+        console.log("ERROR in route handler saving profile picture: ", error);
+        response
+            .statusCode(500)
+            .json({ error: "Problem saving profile picture: " + error });
+    }
 };
 
 const exporting = {
