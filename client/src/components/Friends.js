846c8723f13bac7e197a2e40b4255d110741c6b1
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-20 23:18:10 +0200

    change buttons

--- client/src/components/Friends.js
+++ client/src/components/Friends.js
@@ -1,16 +1,24 @@
-import axios from "../axios";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { receiveFriendsAndPending } from "../actions";
+import {
+    receiveFriendsAndPending,
+    changeFriendpendingToggle,
+} from "../actions";
 import { Link } from "react-router-dom";
 import ProfilePic from "./ProfilePic";
+import FriendButton from "./FriendButton";
 
 export default function Friends() {
+    //pull toggle from store state
+    const friendpending_toggle = useSelector((state) => {
+        return state.friendpending_toggle;
+    });
+
     //pull friends and pending from store state
     const friendsAndPending = useSelector((state) => {
         return state.friendsAndPending;
     });
-    console.log("...(FRIENDS) friendsAndPending: ", friendsAndPending);
+
     //Filter for friends & pending
     const isFriend = (value) => {
         return value.friend_status == "friends";
@@ -25,19 +33,21 @@ export default function Friends() {
             state.friendsAndPending && state.friendsAndPending.filter(isFriend)
         );
     });
-    // console.log("...(FRIENDS) friends: ", friends);
-
     const pending = useSelector((state) => {
         return (
             state.friendsAndPending && state.friendsAndPending.filter(isPending)
         );
     });
-    // console.log("...(FRIENDS) pending: ", pending);
+
+    const onButtonClick = () => {
+        dispatch(changeFriendpendingToggle(friendpending_toggle));
+    };
 
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(receiveFriendsAndPending(friendsAndPending));
+        dispatch(changeFriendpendingToggle(friendpending_toggle));
     }, [dispatch]);
 
     useEffect(() => {
@@ -45,38 +55,99 @@ export default function Friends() {
     }, [pending, friends]);
 
     return (
-        <div>
-            <p>FRIENDS LIST</p>
-            <ul>
-                {pending &&
-                    pending.map((user) => {
-                        return (
-                            <li key={user.id}>
-                                <Link to={"/user/" + user.id}>
-                                    <ProfilePic
-                                        className="avatar"
-                                        profile_url={user.profile_url}
-                                    />
-                                </Link>
-                                <div className="searchResultDetails">
+        <>
+            <section className="customRadioBtn">
+                <div className="buttonsWrapper">
+                    <button
+                        className={
+                            !friendpending_toggle
+                                ? " btnPadding"
+                                : " btnPadding selectedBtn"
+                        }
+                        onClick={onButtonClick}
+                        disabled={friendpending_toggle}
+                    >
+                        <span className="flex">
+                            <i className="material-icons white">people</i>
+                            Friends
+                        </span>
+                    </button>
+                    <button
+                        className={
+                            friendpending_toggle
+                                ? " btnPadding"
+                                : " btnPadding selectedBtn"
+                        }
+                        onClick={onButtonClick}
+                        disabled={!friendpending_toggle}
+                    >
+                        <span className="flex">
+                            <i className="material-icons white">person_add</i>
+                            Pending
+                        </span>
+                    </button>
+                </div>
+            </section>
+            <section className="searchResults">
+                <ul>
+                    {!friendpending_toggle &&
+                        pending &&
+                        pending.map((user) => {
+                            return (
+                                <li key={user.id}>
                                     <Link to={"/user/" + user.id}>
-                                        <h1>
-                                            {user.first_name +
-                                                " " +
-                                                user.last_name}
-                                        </h1>
+                                        <ProfilePic
+                                            className="avatar"
+                                            profile_url={user.profile_url}
+                                        />
                                     </Link>
-                                    <p>{user.bio}</p>
-                                </div>
-                                {/* <FriendButton
-                                    smallButton="smallBtn"
-                                    otherUser_id={user.id}
-                                    onFriendStatusChange=""
-                                ></FriendButton> */}
-                            </li>
-                        );
-                    })}
-            </ul>
-        </div>
+                                    <div className="">
+                                        <Link to={"/user/" + user.id}>
+                                            <h1>
+                                                {user.first_name +
+                                                    " " +
+                                                    user.last_name}
+                                            </h1>
+                                        </Link>
+                                    </div>
+                                    <FriendButton
+                                        smallButton="smallBtn"
+                                        otherUser_id={user.id}
+                                        onFriendStatusChange=""
+                                    ></FriendButton>
+                                </li>
+                            );
+                        })}
+                    {friendpending_toggle &&
+                        friends &&
+                        friends.map((user) => {
+                            return (
+                                <li key={user.id}>
+                                    <Link to={"/user/" + user.id}>
+                                        <ProfilePic
+                                            className="avatar"
+                                            profile_url={user.profile_url}
+                                        />
+                                    </Link>
+                                    <div className="">
+                                        <Link to={"/user/" + user.id}>
+                                            <h1>
+                                                {user.first_name +
+                                                    " " +
+                                                    user.last_name}
+                                            </h1>
+                                        </Link>
+                                    </div>
+                                    <FriendButton
+                                        smallButton="smallBtn"
+                                        otherUser_id={user.id}
+                                        onFriendStatusChange=""
+                                    ></FriendButton>
+                                </li>
+                            );
+                        })}
+                </ul>
+            </section>
+        </>
     );
 }
