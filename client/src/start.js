74e530f4e024cb4e5f9a83ca0ef14a850ed59020
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-20 19:40:22 +0200

    add redux setup, provider, etc.

--- client/src/start.js
+++ client/src/start.js
@@ -2,11 +2,26 @@ import ReactDOM from "react-dom";
 import axios from "./axios";
 import Welcome from "./components/Welcome";
 import App from "./components/App";
+import friendshipReducer from "./reducers/friendshipReducer";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxPromise from "redux-promise";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const store = createStore(
+    friendshipReducer,
+    composeWithDevTools(applyMiddleware(reduxPromise))
+);
 
 axios.get("/api/user/id.json").then(({ data }) => {
     if (!data.userId) {
         ReactDOM.render(<Welcome />, document.querySelector("main"));
     } else {
-        ReactDOM.render(<App />, document.querySelector("main"));
+        ReactDOM.render(
+            <Provider store={store}>
+                <App />
+            </Provider>,
+            document.getElementById("main")
+        );
     }
 });
