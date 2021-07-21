8ae7f33dbd6571dc011e8aaf1034900940d61bc2
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-21 10:58:33 +0200

    add cases for delete, cancel, accept friendship

--- client/src/reducers/friendshipReducer.js
+++ client/src/reducers/friendshipReducer.js
@@ -1,11 +1,38 @@
+/* eslint-disable indent */
 // Reducer
 
 const friendshipReducer = (state = {}, action) => {
+    let updated;
     switch (action.type) {
         case "RECEIVE_FRIENDS_PENDING":
             return { ...state, friendsAndPending: action.payload };
         case "CHANGE_FRIENDPENDING_TOGGLE":
             return { ...state, friendpending_toggle: action.payload };
+        case "DELETE_FRIENDSHIP":
+            updated = state.friendsAndPending.filter(
+                ({ id }) => id != action.payload
+            );
+            return { ...state, friendsAndPending: updated };
+        case "CANCEL_REQUEST":
+            updated = state.friendsAndPending.filter(
+                ({ id }) => id != action.payload
+            );
+            return { ...state, friendsAndPending: updated };
+        case "ACCEPT_FRIENDSHIP":
+            updated = state.friendsAndPending.map((x) => {
+                if (x.id == action.payload) {
+                    x.friend_status = "friends";
+                }
+                return x;
+            });
+            return { ...state, friendsAndPending: updated };
+        // case "REQUEST_FRIENDSHIP":
+        //     return { ...state, friendsAndPending: action.payload };
+        case "RECEIVE_FRIEND_STATUS":
+            return {
+                ...state,
+                friendButton: [{ friend_status: action.payload }],
+            };
         default:
             return state;
     }
