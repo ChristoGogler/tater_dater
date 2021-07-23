8d00cc594f69f622e9a5e2f82797e95a53181064
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-23 21:55:01 +0200

    refactor rendering methods

--- client/src/components/Friends_redux.js
+++ client/src/components/Friends_redux.js
@@ -4,6 +4,7 @@ import {
     receiveFriendsAndPending,
     changeFriendpendingToggle,
     acceptFriendship,
+    requestFriendship,
     deleteFriendship,
     cancelRequest,
 } from "../actions";
@@ -64,8 +65,8 @@ export default function Friends() {
         dispatch(changeFriendpendingToggle(friendpending_toggle));
     }, [dispatch]);
 
-    const renderFriends = () => {
-        return friends.map((user) => {
+    const renderList = (list) => {
+        return list.map((user) => {
             return (
                 <li key={user.id}>
                     <Link to={"/user/" + user.id}>
@@ -79,79 +80,10 @@ export default function Friends() {
                             <h1>{user.first_name + " " + user.last_name}</h1>
                         </Link>
                     </div>
-                    <div className="smallBtn">
-                        <button
-                            className="button submitButton tooltip"
-                            onClick={() => onUnfriendButtonClick(user.id)}
-                        >
-                            <span className="tooltiptext">unfriend</span>
-
-                            <span className="flex">
-                                <i className="material-icons white">
-                                    person_remove
-                                </i>
-                            </span>
-                        </button>
-                    </div>
-                </li>
-            );
-        });
-    };
-
-    const renderPending = () => {
-        return pending.map((user) => {
-            return (
-                <li key={user.id}>
-                    <Link to={"/user/" + user.id}>
-                        <ProfilePic
-                            className="avatar"
-                            profile_url={user.profile_url}
-                        />
-                    </Link>
-                    <div className="searchResultDetails">
-                        <Link to={"/user/" + user.id}>
-                            <h1>{user.first_name + " " + user.last_name}</h1>
-                        </Link>
-                    </div>
-                    <div className="smallBtn">
-                        <button
-                            className="button submitButton tooltip"
-                            onClick={
-                                user.id == user.sender_id
-                                    ? () => onAcceptButtonClick(user.id)
-                                    : () => onCancelButtonClick(user.id)
-                            }
-                        >
-                            <span className="tooltiptext">
-                                {user.id == user.sender_id
-                                    ? "accept"
-                                    : "cancel"}
-                            </span>
-
-                            <span className="flex">
-                                <i className="material-icons white">
-                                    {user.id == user.sender_id
-                                        ? "person_add"
-                                        : "person_add_disabled"}
-                                </i>
-                            </span>
-                        </button>
-
-                        {user.id == user.sender_id && (
-                            <button
-                                className="button submitButton tooltip"
-                                onClick={() => onCancelButtonClick(user.id)}
-                            >
-                                <span className="tooltiptext">reject</span>
-
-                                <span className="flex">
-                                    <i className="material-icons white">
-                                        person_remove
-                                    </i>
-                                </span>
-                            </button>
-                        )}
-                    </div>
+                    {user.friend_status == "pending" &&
+                        renderAcceptRejectButtons(user)}
+                    {user.friend_status == "friends" &&
+                        renderUnfriendButton(user)}
                 </li>
             );
         });
@@ -167,10 +99,70 @@ export default function Friends() {
             </section>
             <section className="searchResults friendsAndPendingList">
                 <ul>
-                    {!friendpending_toggle && pending && renderPending()}
-                    {friendpending_toggle && friends && renderFriends()}
+                    {friendpending_toggle
+                        ? renderList(friends)
+                        : renderList(pending)}
                 </ul>
             </section>
         </>
     );
+
+    function renderAcceptRejectButtons(user) {
+        return (
+            <div className="smallBtn">
+                <button
+                    className="button submitButton tooltip"
+                    onClick={
+                        user.id == user.sender_id
+                            ? () => onAcceptButtonClick(user.id)
+                            : () => onCancelButtonClick(user.id)
+                    }
+                >
+                    <span className="tooltiptext">
+                        {user.id == user.sender_id ? "accept" : "cancel"}
+                    </span>
+
+                    <span className="flex">
+                        <i className="material-icons white">
+                            {user.id == user.sender_id
+                                ? "person_add"
+                                : "person_add_disabled"}
+                        </i>
+                    </span>
+                </button>
+
+                {user.id == user.sender_id && (
+                    <button
+                        className="button submitButton tooltip"
+                        onClick={() => onCancelButtonClick(user.id)}
+                    >
+                        <span className="tooltiptext">reject</span>
+
+                        <span className="flex">
+                            <i className="material-icons white">
+                                person_remove
+                            </i>
+                        </span>
+                    </button>
+                )}
+            </div>
+        );
+    }
+
+    function renderUnfriendButton(user) {
+        return (
+            <div className="smallBtn">
+                <button
+                    className="button submitButton tooltip"
+                    onClick={() => onUnfriendButtonClick(user.id)}
+                >
+                    <span className="tooltiptext">unfriend</span>
+
+                    <span className="flex">
+                        <i className="material-icons white">person_remove</i>
+                    </span>
+                </button>
+            </div>
+        );
+    }
 }
