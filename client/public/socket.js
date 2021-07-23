47a377d13a184648c578561c6f855cce1dcf6f4d
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-23 21:40:19 +0200

    refactor

--- client/public/socket.js
+++ client/public/socket.js
@@ -1,3 +1,5 @@
+//CLIENT
+
 import io from "socket.io-client";
 import { recentMessages, newChatMessage } from "../src/actions/index";
 export let socket;
@@ -6,46 +8,14 @@ export const initialiseSocket = (store) => {
         //First connection
         socket = io.connect();
 
-        socket.emit("connectionCheck", { message: "Hej, wanna connect?" });
-
-        //
-        socket.on("connectionEstablished", ({ message }) => {
-            console.log(`Connected: ${message}`);
-            // do whatever you want with data:
-            // manipulate the DOM
-            // set some state in a react component
-            // dispatch a redux action
-        });
-
         //10 most recent messages
         socket.on("recentMessages", ({ messages }) => {
-            console.log("ON recentMessages: ", messages);
             store.dispatch(recentMessages(messages));
-
-            //TODO: save messages to global state --> then render them
         });
 
         // receiving new message received
-        socket.on("newChatMessage", ({ message, userId }) => {
-            console.log(`incoming chat message from ${userId}: ${message}`);
-            //TODO: save message to global state --> then render it
+        socket.on("newChatMessageToClients", (message) => {
             store.dispatch(newChatMessage(message));
         });
-
-        //sending new message
-        //TODO: grab message from UI --> then emit
-        if (false) {
-            const chatmessage = "message content";
-            const msg = { chatmessage };
-            socket.emit("newChatMessage", msg);
-            // if you want to send some data to the server:
-            // document.querySelector("form").addEventListener("submit", (event) => {
-            //     event.preventDefault();
-            //     console.log("Text: ", event.target.msg.value);
-            //     const message = event.target.msg.value;
-            //     console.log("Message: ", message);
-            //     socket.emit("newChatMessage", { message });
-            // });
-        }
     }
 };
