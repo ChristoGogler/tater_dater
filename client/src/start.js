21ab1ee555726730e997db7de485ad6749252bd7
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-24 12:42:18 +0200

    wrap in Provider

--- client/src/start.js
+++ client/src/start.js
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 import axios from "./axios";
 import Welcome from "./components/Welcome";
 import App from "./components/App";
-import friendshipReducer from "./reducers/friendshipReducer";
+import reducer from "./reducers/reducer";
 
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
@@ -11,13 +11,18 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import { initialiseSocket } from "../public/socket";
 
 const store = createStore(
-    friendshipReducer,
+    reducer,
     composeWithDevTools(applyMiddleware(reduxPromise))
 );
 
-axios.get("/api/user/id.json").then(({ data }) => {
+axios.get("/api/user/id").then(({ data }) => {
     if (!data.userId) {
-        ReactDOM.render(<Welcome />, document.querySelector("main"));
+        ReactDOM.render(
+            <Provider store={store}>
+                <Welcome />
+            </Provider>,
+            document.querySelector("main")
+        );
     } else {
         ReactDOM.render(
             <Provider store={store}>
