f5f496bd6851434cdf21ae632e57cacfdbea1811
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-24 18:18:55 +0200

    class --> functional component

--- client/src/components/Uploader.js
+++ client/src/components/Uploader.js
@@ -1,79 +1,71 @@
 import axios from "../axios";
-import { Component } from "react";
+import { useEffect, useState } from "react";
 
-export default class Uploader extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            file: null,
-            message: "",
-        };
-        this.onPictureUpload = this.onPictureUpload.bind(this);
-        this.onFileinputChange = this.onFileinputChange.bind(this);
-    }
-    async onPictureUpload() {
+export default function Uploader(props) {
+    const [file, setFile] = useState(null);
+    const [message, setMessage] = useState("");
+
+    const onPictureUpload = async () => {
+        console.log("...(onPictureUpload) file: ", file);
+
         event.preventDefault();
         const formData = new FormData();
-        formData.append("file", this.state.file);
+        formData.append("file", file);
         try {
             const result = await axios.post("/api/upload", formData);
-            this.props.onUpload(result.data.user);
-            this.props.hideUploader();
+            props.onUpload(result.data.user);
+            props.hideUploader();
         } catch (error) {
-            this.setState({
+            setMessage({
                 message: "Problem uploading your photo: " + error,
             });
             console.log("ERROR uploading photo: ", error);
         }
-    }
+    };
 
-    async onFileinputChange() {
-        await this.setState({ file: event.target.files[0] });
-        this.onPictureUpload();
-    }
+    const onFileinputChange = async () => {
+        console.log("1", file);
+        await setFile(event.target.files[0]);
+    };
 
-    render() {
-        return (
-            <section className="backdrop">
-                <button
-                    className="closeButton"
-                    onClick={this.props.hideUploader}
+    useEffect(() => {
+        console.log("EFFECT file: ", file);
+        onPictureUpload();
+    }, [file]);
+
+    return (
+        <section className="backdrop">
+            <button className="closeButton" onClick={props.hideUploader}>
+                ×
+            </button>
+
+            <section className="uploadSection">
+                <h1>JJJJ</h1>
+                <h1>Choose a new profile picture!</h1>
+                <form
+                    id="uploadForm"
+                    encType="multipart/form-data"
+                    action="/api/upload"
+                    method="POST"
                 >
-                    ×
-                </button>
-
-                <section className="uploadSection">
-                    <h1>Choose a new profile picture!</h1>
-                    <form
-                        id="uploadForm"
-                        encType="multipart/form-data"
-                        action="/api/upload"
-                        method="POST"
-                    >
-                        <label
-                            className="buttonsWrapper btnPadding"
-                            forhtml="file"
-                        >
-                            <input
-                                id="file"
-                                className="textfield"
-                                type="file"
-                                accept="image/*"
-                                name="file"
-                                onChange={this.onFileinputChange}
-                                required
-                            />
-                            <span className="flex">
-                                <i className="material-icons white">
-                                    add_a_photo
-                                </i>
-                                choose a picture
-                            </span>
-                        </label>
-                    </form>
-                    {this.state.message && <p>{this.state.message}</p>}
-                </section>
+                    <label className="buttonsWrapper btnPadding" forhtml="file">
+                        <input
+                            id="file"
+                            className="textfield"
+                            type="file"
+                            accept="image/*"
+                            name="file"
+                            onChange={onFileinputChange}
+                            required
+                        />
+                        <span className="flex">
+                            <i className="material-icons white">add_a_photo</i>
+                            choose a picture
+                        </span>
+                    </label>
+                </form>
+                {/* {message && <p>{message}</p>} */}
             </section>
-        );
-    }
+        </section>
+    );
 }
