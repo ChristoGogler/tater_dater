03801aec20fc205ba8c19f883c7f496a6117d488
CHRISTO GOGLER <christogogler@gmail.com>
2021-08-11 14:50:05 +0200

    show profileDetails

--- client/src/components/MyProfile.js
+++ client/src/components/MyProfile.js
@@ -1,14 +1,19 @@
+//components
+import BioEditor from "./BioEditor";
+import ProfileBanner from "./ProfileBanner";
+import ProfileDetails from "./ProfileDetails";
+import LightBox from "./LightBox";
+
+//hooks
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
+
+//redux
 import {
     receiveFriendsAndPending,
     toggleLightboxVisible,
     receivePhotoPickerGallery,
 } from "../redux/action-creator";
-import BioEditor from "./BioEditor";
-import ProfileBanner from "./ProfileBanner";
-import ProfileDetails from "./ProfileDetails";
-import LightBox from "./LightBox";
 
 export default function MyProfile() {
     const user = useSelector((state) => {
@@ -43,41 +48,7 @@ export default function MyProfile() {
             <div className="bioContent">
                 <h1>{user.first_name + " " + user.last_name}</h1>
                 <BioEditor></BioEditor>
-                {/* <ProfileDetails userId={user.id} /> */}
-                <div>
-                    <>
-                        <h1>Profile Details</h1>
-                        <p>
-                            <span className="bolder">I am:</span> ...like a
-                            potato - ugly on the outside, yummy in the middle!
-                        </p>
-                        <p>
-                            <span className="bolder">Living in:</span>{" "}
-                            Mühlenbeck-Mönchmühle
-                        </p>
-                        <p>
-                            <span className="bolder">Likes:</span> Big Potatoes,
-                            Sweet Potatoes
-                        </p>
-                        <p>
-                            <span className="bolder">Dislikes :</span> Anything
-                            that's not potatoes!
-                        </p>
-                        <h1>Looking for</h1>
-                        <p>
-                            <span className="bolder">Interested in: </span>
-                            Friends, Dates, Potato Pals
-                        </p>
-                        <p>
-                            <span className="bolder">Gender:</span> male,
-                            female, non-binary, diverse
-                        </p>
-                        <p>
-                            <span className="bolder">Orientation:</span> queer,
-                            yamsexual
-                        </p>
-                    </>
-                </div>
+                <ProfileDetails userId={user.id} />
             </div>
 
             {isLightboxVisible && photos && (
--- client/src/components/ProfileDetails.js
+++ client/src/components/ProfileDetails.js
@@ -1,14 +1,17 @@
+//hooks
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+
+//redux
 import { receiveUserprofile } from "../redux/action-creator";
 
 export default function ProfileDetails(props) {
     const dispatch = useDispatch();
     const userProfile = useSelector((state) => state.userProfile);
-    console.log("...(ProfileDetails) userProfile: ", userProfile);
+
     useEffect(() => {
         dispatch(receiveUserprofile(props.userId));
-    }, []);
+    }, [props.userId]);
 
     return (
         <div>
@@ -44,40 +47,6 @@ export default function ProfileDetails(props) {
                     <p>
                         <span className="bolder">Orientation:</span>{" "}
                         {userProfile.orientation}
-                    </p>
-                </>
-            )}
-            {userProfile == null && (
-                <>
-                    <h1>Profile Details</h1>
-                    <p>
-                        <span className="bolder">I am:</span> ...like a potato -
-                        ugly on the outside, yummy in the middle!
-                    </p>
-                    <p>
-                        <span className="bolder">Living in:</span>{" "}
-                        Mühlenbeck-Mönchmühle
-                    </p>
-                    <p>
-                        <span className="bolder">Likes:</span> Big Potatoes,
-                        Sweet Potatoes
-                    </p>
-                    <p>
-                        <span className="bolder">Dislikes :</span> Anything
-                        that's not potatoes!
-                    </p>
-                    <h1>Looking for</h1>
-                    <p>
-                        <span className="bolder">Interested in: </span>
-                        Friends, Dates, Potato Pals
-                    </p>
-                    <p>
-                        <span className="bolder">Gender:</span> male, female,
-                        non-binary, diverse
-                    </p>
-                    <p>
-                        <span className="bolder">Orientation:</span> queer,
-                        yamsexual
                     </p>
                 </>
             )}
