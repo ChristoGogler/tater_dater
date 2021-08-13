da2c86904cdd7507a860228267d04ff267b9e372
CHRISTO GOGLER <christogogler@gmail.com>
2021-08-13 20:51:53 +0200

    refactor friendshipRoutes

---
+++ server/routes/friendshipRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const friendshipRouter = express.Router();
+
+const {
+    changeFriendStatus,
+    getFriendStatus,
+    getFriendList,
+    getFriendListById,
+} = require("../routehandler");
+
+//GET FRIENDSHIP STATUS
+friendshipRouter.get("/status/:user_id", getFriendStatus);
+
+//CHANGE FRIEND STATUS
+friendshipRouter.post("/request", changeFriendStatus);
+
+//GET FRIENDS AND PENDING
+friendshipRouter.get("/friends", getFriendList);
+
+//GET OTHER USERS FRIENDS
+friendshipRouter.get("/friends_by_id/:id", getFriendListById);
+
+module.exports = friendshipRouter;
